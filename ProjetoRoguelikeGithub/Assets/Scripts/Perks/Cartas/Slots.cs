5440a1ff54bac3f7c9db47d793cae6828a0cc626
DijuBR <jpdiju@gmail.com>
2023-06-20 21:45:13 -0300

    Inicialização das Cartas

--- ProjetoRoguelikeGithub/Assets/Scripts/Perks/Cartas/Slots.cs
+++ ProjetoRoguelikeGithub/Assets/Scripts/Perks/Cartas/Slots.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Slots : MonoBehaviour
 {
+    private RectTransform pos;
+    [SerializeField] private GameObject[] cartas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pos = GetComponent<RectTransform>();
+        int r = Random.Range(0, cartas.Length);
+        Instantiate(cartas[r], pos);
     }
 
     // Update is called once per frame
