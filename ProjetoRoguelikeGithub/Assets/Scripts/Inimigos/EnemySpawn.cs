9697ae6f2157cc4f167be7778d4b6f9cecfedf51
VorteZzz <gabrielferreiradasilva055@gmail.com>
2023-08-01 22:02:35 -0300

    tentando arrumar o spawn

--- ProjetoRoguelikeGithub/Assets/Scripts/Inimigos/EnemySpawn.cs
+++ ProjetoRoguelikeGithub/Assets/Scripts/Inimigos/EnemySpawn.cs
@@ -9,6 +9,9 @@ public class EnemySpawn : MonoBehaviour
     public Transform pos;
     public int inimigosnascidos;
 
+    private int spawn;
+    public int addSpawn;
+
     void Spawn()
     {
         int randomEnemy = Random.Range(0, enemyPrefabs.Length);
@@ -16,10 +19,30 @@ public class EnemySpawn : MonoBehaviour
             new Vector3(Random.Range(pos.position.x - 2f, pos.position.x + 2f),
             Random.Range(pos.position.y - 2f, pos.position.y + 2f), 0),
             Quaternion.identity);
+    } 
+
+    void acrescentaSpawn()
+    {
+        if (GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().score > 0 && GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().score < 5)
+        {
+            addSpawn = Random.Range(1, 2);
+        }
+
+        if (GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().score >= 5 && GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().score < 13)
+        {
+            addSpawn = Random.Range(2, 4);
+        }
+
+        if (GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().score >= 13)
+        {
+            addSpawn = Random.Range(3, 5);
+        }
     }
+
     private void Start()
     {
-        int spawn = Random.Range(1,5);
+        acrescentaSpawn();
+        spawn = Random.Range(2, 4) + addSpawn;
         int spawnMax = 0;
 
         while(spawnMax != spawn)
@@ -30,12 +53,9 @@ public class EnemySpawn : MonoBehaviour
             Debug.Log(spawnMax);
         }
     }
+
     private void Update()
     {
         
-        if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            
-        }
     }
 }
