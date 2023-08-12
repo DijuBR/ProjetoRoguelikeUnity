77d0be36256cdf60901610a140e71f5a05f7a5ca
VorteZzz <gabrielferreiradasilva055@gmail.com>
2023-08-12 17:20:19 -0300

    mais atualizações

--- ProjetoRoguelikeGithub/Assets/Scripts/Portal/PassarFase.cs
+++ ProjetoRoguelikeGithub/Assets/Scripts/Portal/PassarFase.cs
@@ -26,13 +26,13 @@ public class PassarFase : MonoBehaviour
                 GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().spawn = Random.Range(3, 4);
                 break;
             case (1):
-                GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().spawn = Random.Range(4, 5);
+                GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().spawn = Random.Range(3, 5);
                 break;
             case (2):
-                GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().spawn = Random.Range(5, 6);
+                GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().spawn = Random.Range(4, 6);
                 break;
             case (3):
-                GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().spawn = Random.Range(6, 7);
+                GameObject.Find("PlayerTeste").GetComponent<PlayerStatus>().spawn = Random.Range(5, 7);
                 break;
         }
     }
